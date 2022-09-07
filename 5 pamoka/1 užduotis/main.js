62acc314abafb14ffae822ee81b416099f2ebf24
TomasPerminas <tomas.perminas@gmail.com>
2022-09-07 12:04:04 +0300

    5 pamoka

--- 5 pamoka/1 užduotis/main.js
+++ 5 pamoka/1 užduotis/main.js
@@ -1,6 +1,6 @@
-//============= 1 užduotis ============
+//============ 1 užduotis ============
 
-console.log("============= 1 užduotis ===========");
+console.log("============= 1 užduotis =============");
 
 let rndText1 = "Keanu";
 let rndText2 = "Reeves";
@@ -16,7 +16,7 @@ if (length1 > length2) {
 
 //============ 2 užduotis ============
 
-console.log("=========== 2 užduotis ============");
+console.log("============= 2 užduotis =============");
 
 let rndText3 = "Keanu";
 let rndText4 = "Reeves";
@@ -24,10 +24,12 @@ let rndText4 = "Reeves";
 let uppercase = rndText3.toUpperCase();
 let lowercase = rndText4.toLowerCase();
 
-console.log(uppercase);
-console.log(lowercase);
+console.log(uppercase + " " + lowercase);
 
-//============ 3 užduotis ===========
+//=========== 3 užduotis =============
 
-console.log("============= 3 užduotis ============");
+console.log("============= 3 užduotis =============");
+
+let rndText5 = "Keanu";
+let rndText6 = "Reeves";
 
