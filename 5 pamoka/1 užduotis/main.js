aa4a573af8011be4b9c29ba402eb364213c0cdb5
TomasPerminas <tomas.perminas@gmail.com>
2022-09-07 11:49:50 +0300

    5 pamoka

---
+++ 5 pamoka/1 užduotis/main.js
@@ -0,0 +1,20 @@
+//============= 1 užduotis ============
+
+console.log("============= 1 užduotis ===========");
+
+let rndText1 = "Keanu";
+let rndText2 = "Reeves";
+
+let length1 = rndText1.length;
+let length2 = rndText2.length;
+
+if (length1 > length2) {
+    console.log(rndText1)
+} else {
+    console.log(rndText2)
+};
+
+//============ 2 užduotis ============
+
+console.log("=========== 2 užduotis ============");
+
