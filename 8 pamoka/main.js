e9b8b59b9b9381707e164454055518b9e85fe705
TomasPerminas <tomas.perminas@gmail.com>
2022-09-13 08:50:46 +0300

    8 pamoka

---
+++ 8 pamoka/main.js
@@ -0,0 +1,19 @@
+//============= LENGVESNI ============
+
+console.log("============== LENGVESNI =============");
+
+//============ 1 užduotis ============
+
+console.log("============= 1 užduotis =============");
+
+let result = exponent(2, 3);
+console.log(result);
+
+function exponent (a, b) {
+    return Math.pow(a, b);
+}
+
+//============ 2 užduotis ============
+
+console.log("============= 2 užduotis =============");
+
