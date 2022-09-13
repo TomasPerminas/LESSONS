34996b79f877a1084a93763776a82aad20586a3a
TomasPerminas <tomas.perminas@gmail.com>
2022-09-13 12:18:30 +0300

    8 pamoka

--- 8 pamoka/main.js
+++ 8 pamoka/main.js
@@ -6,14 +6,81 @@ console.log("============== LENGVESNI =============");
 
 console.log("============= 1 užduotis =============");
 
+let multiply = numbers(2, 3);
+
+function numbers(a, b) {
+    return a * b;
+
+}
+
+console.log(multiply);
+
+//============ 2 užduotis ============
+
+console.log("============= 2 užduotis =============");
+
+let myName = "Tomai";
+
+greeting(myName);
+
+function greeting(myName) {
+    console.log("Labas " + myName);
+
+}
+
+//============ 3 užduotis ============
+
+console.log("============= 3 užduotis =============");
+
+myText("Geriau eičiau fūrą vairuot")
+
+function myText(text) {
+    console.log(text.length);
+
+}
+
+//============ 4 užduotis ============
+
+console.log("============= 4 užduotis =============");
+
+let firstName = "tomas";
+let lastName = "perminas";
+
+names(firstName, lastName);
+
+function names(firstName, lastName) {
+    console.log(firstName[0].toUpperCase(0) + lastName[0].toUpperCase(0));
+
+}
+
+//============ 5 užduotis ============
+
+console.log("============= 5 užduotis =============");
+
+
+
+//============= VIDUTINIAI ============
+
+console.log("============== VIDUTINIAI =============");
+
+//============ 1 užduotis ============
+
+console.log("============= 1 užduotis =============");
+
 let result = exponent(2, 3);
 console.log(result);
 
-function exponent (a, b) {
+function exponent(a, b) {
     return Math.pow(a, b);
 }
 
 //============ 2 užduotis ============
 
 console.log("============= 2 užduotis =============");
+
+document.getElementById("title").innerText = "Atsakymas yra:" + " " + result;
+
+//============ 3 užduotis ============
+
+console.log("============= 3 užduotis =============");
 
