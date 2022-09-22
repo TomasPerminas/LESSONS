892dc173e755aa6d5c749a278bdb1b9816ec328c
TomasPerminas <tomas.perminas@gmail.com>
2022-09-22 13:55:52 +0300

    13 pamoka

---
+++ 11-12 pamoka/index.php
@@ -0,0 +1,11 @@
+<?php include "./header.php" ?>
+
+
+<a href="./index.php">Change to black</a>
+<a href="./red.php">Change to red</a>
+
+<body style="background-color: black">;
+
+</body>
+
+</html>
