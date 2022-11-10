8fceaa72fe5fc5b861312f8c04d3075d91b8503e
TomasPerminas <tomas.perminas@gmail.com>
2022-11-10 18:54:49 +0200

    changes

---
+++ Vilnius Coding School/PYTHON/11 pamoka/task1.py
@@ -0,0 +1,33 @@
+# Sukurkite ir išveskite tokius nurodytus ranges:
+# [0, 1, 2, 3, 4, 5, 6]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# [20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+# [1, 3, 5, 7, 9]
+# [30, 33, 36, 39, 42, 45, 48]
+# [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+
+num1 = list(range(7))
+print(num1)
+
+num2 = list(range(1,10))
+print(num2)
+
+num3 = list(range(2,16))
+print(num3)
+
+num4 = list(range(20,31))
+print(num4)
+
+num5 = list(range(20,41,2))
+print(num5)
+
+num6 = list(range(1,10,2))
+print(num6)
+
+num7 = list(range(30,49,3))
+print(num7)
+
+num8 = list(range(50,101,5))
+print(num8)
---
+++ Vilnius Coding School/PYTHON/11 pamoka/task2.py
@@ -0,0 +1,25 @@
+# Sukurkite ir išveskite tokius nurodytus ranges:
+# [1, 8, 15, 22, 29, 36, 43, 50, 57, 64, 71, 78, 85, 92, 99]
+# [3, 12, 21, 30, 39, 48, 57, 66, 75, 84, 93]
+# [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+# [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# [50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
+
+num1 = list(range(1,100,7))
+print(num1)
+
+num2 = list(range(3,94,9))
+print(num2)
+
+num3 = list(range(10,0,-1))
+print(num3)
+
+num4 = list(range(100,9,-10))
+print(num4)
+
+num5 = list(range(10,-1,-1))
+print(num5)
+
+num6 = list(range(50,4,-5))
+print(num6)
