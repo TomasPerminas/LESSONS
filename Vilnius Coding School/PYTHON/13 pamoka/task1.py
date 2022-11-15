1b74a93d655fc83d2bdfd620f83c83a25db033ca
TomasPerminas <tomas.perminas@gmail.com>
2022-11-15 19:02:04 +0200

    changes

---
+++ Vilnius Coding School/PYTHON/13 pamoka/task1.py
@@ -0,0 +1,8 @@
+# Susikurkite list vardams saugoti ir užpildykite jį informacija. Išveskite visą šį list, tuomet pirmą narį iš jo, paskutinį narį, bei kiek narių jame yra.
+
+names = ['Tomas', 'Mantas', 'Jonas', 'Petras']
+
+print(names)
+print(names[0])
+print(names[3])
+print(len(names))
---
+++ Vilnius Coding School/PYTHON/13 pamoka/task2.py
@@ -0,0 +1,6 @@
+# Susikurkite list žmonių ūgiams saugoti ir užpildykite jį informacija. Išveskite viso šio list duomenis bei kiek ūgių turite.
+
+height = [160, 170, 180, 190]
+print(height)
+
+print(len(height))
