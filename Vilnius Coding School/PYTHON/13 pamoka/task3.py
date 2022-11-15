ba8ec41017e318a7dac166b8dcbef059e98d3cc2
TomasPerminas <tomas.perminas@gmail.com>
2022-11-15 19:16:57 +0200

    changes

--- Vilnius Coding School/PYTHON/13 pamoka/task3.py
+++ Vilnius Coding School/PYTHON/13 pamoka/task3.py
@@ -1,2 +1,8 @@
 # Susikurkite list pažymiams saugoti. Pamėginkite sukurti programą, kur vartotojas galėtų suvesti norimą kiekį naujų duomenų. Galiausiai išveskite visus pažymius ir jų kiekį.
 
+grade = []
+
+print('Iveskite studento bala: ')
+grade.append(int(input()))
+
+print('Studentu ivertinima:', grade)
